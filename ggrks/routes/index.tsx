c9b54d5f789fb17e7ff3295fa409d189eaaff073
a-aaruu1 <146954010+a-aaruu1@users.noreply.github.com>
2024-02-24 15:51:20 +0900

    Update index.tsx

--- ggrks/routes/index.tsx
+++ ggrks/routes/index.tsx
@@ -7,12 +7,12 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>ggrks</title>
+        <title>めーぷる</title>
       </Head>
       <div class="px-4 py-8 mx-auto bg-[#86efac]">
         <h1>https://mei.deno.dev/[ググらせたい内容]</h1>
         <p>空白は+です。</p>
-        <p>by a_aaruu / <a href="https://www.burgerking.co.jp">@BK</a></p>
+        <p>by めーぷる / <a href="https://www.burgerking.co.jp">@めーぷる</a></p>
       </div>
     </>
   );
